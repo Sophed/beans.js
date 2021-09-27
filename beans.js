8d6403046d3f30d4d2d839715b13c3fb1f7577cd
Lily <65602967+LilyyDev@users.noreply.github.com>
2021-09-27 15:55:08 +0100

    the beans
    they're here. run.

---
+++ beans.js
@@ -0,0 +1,5 @@
+var imgs = document.getElementsByTagName("img");
+for(var i=0, l=imgs.length; i<l; i++) {
+imgs[i].src = "https://static.independent.co.uk/2021/01/04/09/iStock-969582980.jpg?width=982&height=726&auto=webp&quality=75";
+}
+// i have brain damage
